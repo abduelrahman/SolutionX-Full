fca0f6bbe4a65fef211d141a1fc469686119f19c
abduelrahman <a.emam@sbs-sa.net>
2024-03-07 15:06:40 +0200

    - Upgrade.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
 android {
     namespace = "am.leon.solutionx"
+    flavorDimensions += "logging"
     compileSdk = 34
 
     defaultConfig {
@@ -21,28 +22,57 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+
+    productFlavors {
+        create("logCat") {
+            dimension = "logging"
+        }
+
+        create("logWriter") {
+            dimension = "logging"
+        }
+
+        create("production") {
+            dimension = "logging"
+        }
     }
+
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
+    // Unit Test
     testImplementation(libs.junit)
+
+    // Android Test
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Android
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+
+    // Material
+    implementation(libs.material)
+
+    // Kotlin Reflect
+    implementation(libs.kotlin.reflect)
 }
