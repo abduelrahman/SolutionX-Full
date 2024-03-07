2ef571a29fac6fd1b9109d3df2d8b8b043de5246
abduelrahman <a.emam@sbs-sa.net>
2024-03-07 15:05:13 +0200

    - This task is created to demonstrate the advanced way to achieve the correct way for product flavors with the logging feature.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
 android {
     namespace = "am.leon.solutionx"
+    flavorDimensions += "logging"
     compileSdk = 34
 
     defaultConfig {
@@ -26,12 +27,46 @@ android {
             )
         }
     }
+
+    productFlavors {
+        create("logCat") {
+            dimension = "logging"
+        }
+
+        create("logWriter") {
+            dimension = "logging"
+        }
+
+        create("production") {
+            dimension = "logging"
+        }
+    }
+
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    androidComponents {
+        beforeVariants { variant ->
+            val isReleaseWithLogCatOrLogWriterFlavor = variant.buildType == "release" &&
+                    variant.productFlavors.any { it.second in listOf("logCat", "logWriter") }
+
+            val isDebugWithProductionFlavor =
+                variant.buildType == "debug" && variant.productFlavors.any { it.second == "production" }
+
+            if (isReleaseWithLogCatOrLogWriterFlavor || isDebugWithProductionFlavor) {
+                variant.enable = false
+            }
+        }
     }
 }
 
